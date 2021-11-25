c464fa2131d5de92154dd46c66dab8c770f3f066
grzeszut <grzeszut@LCE47133>
2021-11-25 11:56:45 +0100

    first commit

---
+++ GitHubTestRepository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubTestRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
